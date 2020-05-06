1f4657a0dfba30603caa1b6946fa74ab37d204e0
minji.Choi <minjichoi88@naver.com>
2020-05-06 12:46:08 +0900

    test 수정

--- Test.java
+++ Test.java
@@ -2,6 +2,8 @@ class Test{
 	public static void main(String [] args){
 		System.out.println("Hello Git");
 		System.out.println("Hi git");
+		System.out.println("branch1");
+		System.out.println("branch2");
 	}
 
 }
